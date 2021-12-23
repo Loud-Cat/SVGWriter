f586bddbc1580ce0afb3373da5504c46ea3c30bd
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-23 14:45:07 -0600

    Create writer.py

---
+++ writer.py
@@ -0,0 +1,15 @@
+class SVG:
+    def __init__(self, filename="file.svg", width=500, height=500):
+        self.filename = filename
+        self.lines = [f'<svg width="{width}" height="{height}" xmlns="http://www.w3.org/2000/svg">']
+
+    def append(self, shape):
+        self.lines.append(shape)
+
+    def __repr__(self):
+        return "\n".join(self.lines + ["</svg>"])
+    __str__ = __repr__
+
+    def write(self):
+        with open(self.filename, "w") as svg:
+            svg.write( str(self) )
