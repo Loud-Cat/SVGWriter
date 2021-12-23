e335047013044cd8172186e598f7de8ec0b6ada7
Edison Ditto <73842360+MedisonMan@users.noreply.github.com>
2021-12-23 14:15:04 -0600

    Create shapes.py

---
+++ shapes.py
@@ -0,0 +1,19 @@
+def Rect(x,y, w,h, fill="black", stroke="none", sw=0):
+    return f'<rect x="{x}" y="{y}" width="{w}" height="{h}" fill="{fill}" stroke="{stroke}" stroke-width="{sw}" />'
+
+def Circle(cx,cy, r, fill="black", stroke="none", sw=0):
+    return f'<circle cx="{cx}" cy="{cy}" r="{r}" fill="{fill}" stroke="{stroke}" stroke-width="{sw}" />'
+
+def Ellipse(cx,cy, rx,ry, fill="black", stroke="none", sw=0):
+    return f'<ellipse cx="{cx}" cy="{cy}" rx="{rx}" ry="{ry}" fill="{fill}" stroke="{stroke}" stroke-width="{sw}" />'
+
+def Line(x1,y1, x2,y2, stroke="black", sw=1):
+    return f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" stroke="{stroke}" stroke-width="{sw}" />'
+
+def Polyline(points, fill="none", stroke="black", sw=1):
+    newpoints = " ".join([f"{x},{y}" for x,y in points])
+    return f'<polyline points="{newpoints}" fill="{fill}" stroke="{stroke}" stroke-width="{sw}" />'
+
+def Polygon(points, fill="none", stroke="black", sw=1):
+    newpoints = " ".join([f"{x},{y}" for x,y in points])
+    return f'<polygon points="{newpoints}" fill="{fill}" stroke="{stroke}" stroke-width="{sw}" />'
